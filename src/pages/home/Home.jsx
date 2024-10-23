6b05bce7f47422fa05fcff9fb2f6185ea7d9a0f4
Asad Ali Asim <asadaliasim909@gmail.com>
2024-10-23 19:13:17 +0500

    home img

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,16 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className="h-screen relative bg-gray-800 text-white flex items-center justify-between w-[100%] flex-col">
-      <div className="relative z-10 text-center py-24  ">
+    <div
+      className="h-screen relative flex items-center justify-between w-[100%] flex-col"
+      style={{
+        backgroundImage:
+          'url("https://images.unsplash.com/photo-1577278254637-a8b6a7f3614e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <div className="relative z-10 text-center py-24">
         <h1 className="text-5xl font-bold mb-4">
           Discover Detroits Hidden Gems
         </h1>
@@ -18,7 +26,5 @@ const Home = () => {
     </div>
   );
 };
-
-//
 
 export default Home;
