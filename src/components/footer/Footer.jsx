9775fe0c667800abbef1f4601bdea9bf29dd9a1c
sajjad karim <sajukarim76@gmail.com>
2024-10-24 14:56:47 +0500

    added: term and condition, privacy and polcy, guide pages

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -7,7 +7,7 @@ const Footer = () => {
         <h2 className="text-xl font-bold mb-4">Ready to Explore Detroit?</h2>
         <p>Join DEW today and start planning your unforgettable experiences.</p>
 
-        <Link to={'/register'}>
+        <Link to={"/register"}>
           <button className="bg-black text-white py-2 px-4 mt-4">
             Sign Up Now
           </button>
