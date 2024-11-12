9b3e5005b81d6f8871f4f837ea4a2f3371efb1c3
FIRST_NAME LAST_NAMEAnasPirzada <anaspirzadaiub@gmail.com>
2024-11-13 02:51:05 +0500

    loader fixed

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,21 +1,18 @@
-const loader = () => {
+import React from 'react';
+
+const Loader = ({ width = 'w-36', height = 'h-4', color = 'bg-gray-300', spacing = 'space-y-2', round = 'rounded' }) => {
   return (
-    <div>
-      <div className='animate-pulse space-y-4'>
-        <div className='flex items-center space-x-4'>
-          <div className='bg-gray-300 rounded-full h-20 w-20'></div>
-          <div className='space-y-2'>
-            <div className='h-4 bg-gray-300 rounded w-36'></div>
-            <div className='h-4 bg-gray-300 rounded w-48'></div>
-          </div>
-        </div>
-        <div className='space-y-2'>
-          <div className='h-4 bg-gray-300 rounded w-32'></div>
-          <div className='h-4 bg-gray-300 rounded w-24'></div>
-          <div className='h-4 bg-gray-300 rounded w-20'></div>
+    <div className="animate-pulse flex justify-center items-center">
+      <div className="flex flex-col space-x-4">
+      
+        <div className={spacing}>
+          <div className={`${height} ${color} ${round} ${width}`}></div>
+          <div className={`${height} ${color} ${round} ${width}`}></div>
+          <div className={`${height} ${color} ${round} ${width}`}></div>
         </div>
       </div>
     </div>
   );
 };
-export default loader;
+
+export default Loader;
